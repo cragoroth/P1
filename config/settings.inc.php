616936f019d6a197984cc40445e09a420d0d7a3c
Emeric <emeric.rebillard@free.fr>
2011-11-10 16:56:22 +0100

    Premier commit

---
+++ config/settings.inc.php
@@ -0,0 +1,22 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_TYPE_', 'MySQL');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '0000');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('__PS_BASE_URI__', '/');
+define('_PS_CACHING_SYSTEM_', 'MCached');
+define('_PS_CACHE_ENABLED_', '0');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_THEME_NAME_', 'prestashop');
+define('_COOKIE_KEY_', 'Gw1II86wOqpN6GMhNPdHEQL83QIJOQSUBywGxbaQbqnBkAg4AQhxAoNH');
+define('_RIJNDAEL_KEY_', 'iZHg66gZ4VYyKTeySqS0SacQO7Ut38ya');
+define('_COOKIE_IV_', 'MmAPDFbe');
+define('_RIJNDAEL_IV_', '0rKNJg0hUwXj5tGJrwNZvw==');
+define('_PS_CREATION_DATE_', '2011-11-09');
+define('_PS_VERSION_', '1.4.5.1');
+?>
---
+++ tools/smarty/compile/59c9ad001301ef19e19d9bb2a019822af2435284.file.index.tpl.php
@@ -0,0 +1,22 @@
+<?php /* Smarty version Smarty-3.0.7, created on 2011-11-10 09:31:52
+         compiled from "/home/www/site2/themes/prestashop/index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:1140178174ebb8bf8785c50-65657997%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '59c9ad001301ef19e19d9bb2a019822af2435284' => 
+    array (
+      0 => '/home/www/site2/themes/prestashop/index.tpl',
+      1 => 1306138304,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1140178174ebb8bf8785c50-65657997',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+
+
+<?php echo $_smarty_tpl->getVariable('HOOK_HOME')->value;?>
